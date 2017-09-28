b80fbb893ae07311eb9f37e78cf9aacd79e1b7af
Randal Cunanan <randal@randalvance.net>
2017-09-29 07:40:16 +0800

    Prevent broadcast receiver from being called twice.

--- src/CodeQuasar.Xamarin.Plugins.Location.Android/LocationStatusBroadcastReceiver.cs
+++ src/CodeQuasar.Xamarin.Plugins.Location.Android/LocationStatusBroadcastReceiver.cs
@@ -10,9 +10,20 @@ namespace CodeQuasar.Xamarin.Plugins.Location
     [IntentFilter(new[] { LocationManager.ProvidersChangedAction })]
     public class LocationStatusBroadcastReceiver : BroadcastReceiver, ILocationStatusSource
     {
+        private static bool? PreviousState = null;
+
         public override void OnReceive(Context context, Intent intent)
         {
-            MessagingCenter.Send<ILocationStatusSource>(this, MessengerKeys.LocationStatusBroadcastReceived);
+            LocationManager manager = (LocationManager)context.GetSystemService(Context.LocationService);
+            var locationEnabled = manager.IsProviderEnabled(LocationManager.GpsProvider);
+            
+            // Circumvent android bug where broadcast receiver is called twice on some divices
+            if (!PreviousState.HasValue || PreviousState.Value != locationEnabled)
+            {
+                PreviousState = locationEnabled;
+
+                MessagingCenter.Send<ILocationStatusSource>(this, MessengerKeys.LocationStatusBroadcastReceived);
+            }
         }
     }
 }
