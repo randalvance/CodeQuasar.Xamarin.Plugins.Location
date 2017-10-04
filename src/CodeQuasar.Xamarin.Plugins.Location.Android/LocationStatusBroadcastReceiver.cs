e6f3c39dc5e91adc0ccdc65274b881f7679dde22
Randal Vance Cunanan <randalvance@users.noreply.github.com>
2017-10-04 08:08:31 +0800

    Only perform action if location was actually changed. (#3)
    Only perform action if location was actually changed.

--- src/CodeQuasar.Xamarin.Plugins.Location.Android/LocationStatusBroadcastReceiver.cs
+++ src/CodeQuasar.Xamarin.Plugins.Location.Android/LocationStatusBroadcastReceiver.cs
@@ -2,6 +2,8 @@
 using Android.Content;
 using Android.Locations;
 using CodeQuasar.Xamarin.Plugins.Location.Abstractions;
+using System.Threading;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace CodeQuasar.Xamarin.Plugins.Location
@@ -10,9 +12,18 @@ namespace CodeQuasar.Xamarin.Plugins.Location
     [IntentFilter(new[] { LocationManager.ProvidersChangedAction })]
     public class LocationStatusBroadcastReceiver : BroadcastReceiver, ILocationStatusSource
     {
+        private static bool? oldLocationEnabled = null;
+
         public override void OnReceive(Context context, Intent intent)
         {
-            MessagingCenter.Send<ILocationStatusSource>(this, MessengerKeys.LocationStatusBroadcastReceived);
+            var locationManager = (LocationManager)context.GetSystemService(Context.LocationService);
+            var newLocationEnabled = locationManager.IsProviderEnabled(LocationManager.GpsProvider);
+
+            if (!oldLocationEnabled.HasValue || oldLocationEnabled.Value != newLocationEnabled)
+            {
+                oldLocationEnabled = newLocationEnabled;
+                MessagingCenter.Send<ILocationStatusSource>(this, MessengerKeys.LocationStatusBroadcastReceived);
+            }
         }
     }
 }
